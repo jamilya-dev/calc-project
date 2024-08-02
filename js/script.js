2b37892c809aeb2b7b1256a4a3cbf75cb048046e
jamilya-dev <omg.groups@yandex.ru>
2024-08-02 13:34:26 +0300

    доработка задания 9

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const title = document.getElementsByTagName('h1')[0];
-const btns = document.getElementsByClassName('handler_btn');
+const btn1 = document.getElementsByClassName('handler_btn')[0];
+const btn2 = document.getElementsByClassName('handler_btn')[1];
 const btnPlus = document.querySelector('.screen-btn');
 const otherItemsPercent = document.querySelectorAll('.other-items.percent');
 const otherItemsNumber = document.querySelectorAll('.other-items.number');
